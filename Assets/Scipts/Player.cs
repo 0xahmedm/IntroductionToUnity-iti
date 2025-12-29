18c184c7573d59620e361f82829120033309e314
Ahmed Mahmoud <ahmedmahmoud15520034@gmail.com>
2025-12-29 18:55:56 +0200

    -

---
+++ Assets/Scipts/Player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        player.SetActive(true);
+    }
+
+    void Update()
+    {
+        
+    }
+}
